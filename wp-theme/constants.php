63538529c7a57ffda2011d8cf01d857de1f5fc40
Kate Hart <kejt.hart@gmail.com>
2013-08-01 20:31:48 -0500

    Added flag for using development or production settings

--- wp-theme/constants.php
+++ wp-theme/constants.php
@@ -9,11 +9,30 @@
  * @author Nate Hart (and future CIFers)
  */
 
+/**
+ * STYLESHEET_URL: The location of the theme's stylesheet.
+ *                 We don't use WordPress's style.css for more
+ *                 portability and caching from our CDN.
+ * FAVICON_URL: The location of the theme's favicon.
+ */
+
 // The location of the theme directory
 define( 'TEMPLATE_DIRECTORY', get_template_directory_uri() );
 
-// The location of the stylesheet to use with this theme
-define( 'STYLESHEET_URL', TEMPLATE_DIRECTORY . '/../../../../Website/cdn/css/style.css' );
+// Whether special debugging settings should be used
+define( 'THEME_DEBUG', true );
 
-// The location of the favicon to use with this theme
-define( 'FAVICON_URL', TEMPLATE_DIRECTORY . '/../../../../Website/cdn/favicon.ico' );
+if ( THEME_DEBUG ) {
+	// Theme debugging/development settings
+
+	define( 'STYLESHEET_URL', TEMPLATE_DIRECTORY . '/../../../../Website/cdn/css/style.css' );
+
+	define( 'FAVICON_URL', TEMPLATE_DIRECTORY . '/../../../../Website/cdn/favicon.ico' );
+} else {
+	// Settings for the live site
+	
+	define( 'STYLESHEET_URL', 'cdn.cif.rochester.edu/style.css' );
+
+	define( 'FAVICON_URL', 'cdn.cif.rochester.edu/favicon.ico' );
+}
+
