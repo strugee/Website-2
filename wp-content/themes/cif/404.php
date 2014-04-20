1e6895bcc33979e1114e342a6df05f27acde65cd
Kate Hart <kejt.hart@gmail.com>
2014-04-20 14:54:21 -0400

    Added cat picture to 404 page

--- wp-content/themes/cif/404.php
+++ wp-content/themes/cif/404.php
@@ -13,6 +13,9 @@ get_header(); ?>
 
 	<div class="two-columns">
 		<p class="column">Sorry, we couldn't find what you were looking for. You could try starting from our <a href="<?php echo home_url(); ?>" rel="home">homepage</a>, or maybe what you're looking for is part of <a href="<?php echo PANEL_URL; ?>">panel</a>.</p>
+		<div class="column">
+		<img src="<?php echo TEMPLATE_DIRECTORY; ?>/images/computer-cat.jpg" alt="A cat playing inside of an empty computer" />
+		</div>
 	</div>
 </div>
 <?php get_footer();
